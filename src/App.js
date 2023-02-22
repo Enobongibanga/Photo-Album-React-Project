722a352bed52a6a485920c8e794cd7f7c3aa2c98
Enobongibanga <enobongibanga7@gmail.com>
2023-02-23 00:51:50 +0100

    the clean template is ready

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const App = () => {
+    return (
+        <div>
+            <h1>Photo-Album-React-Project  </h1>
+            <p> contributors: Mercy, Enobong and BabaTunde</p>
+        </div>
+    );
+};
+
+export default App;
