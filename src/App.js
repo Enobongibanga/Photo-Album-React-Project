1bdb42072b224e102b7164428171e31936933243
Enobongibanga <enobongibanga7@gmail.com>
2023-02-24 19:57:15 +0100

    dummy meal card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import React from 'react';
+// import React, { useState } from 'react';
 
+// import { getUniqueId } from './utility/uniqueId';
+// import NewProduct from './components/products/NewProduct';
+// import Products from './components/products/Products';
+
+// const productsDummyData = [
+//     {
+//         id: getUniqueId(),
+//         title: 'product a',
+//         price: 32.5,
+//         category: 'men',
+//         description: 'dummy description goes here'
+//     }
+// ];
+
+// const App = () => {
+//     const [products, setProducts] = useState(productsDummyData);
+
+//     const handleAddProduct = (value) => {
+//         const newProduct = { id: getUniqueId(), ...value };
+//         // update the products state with the newProduct
+//         newProduct.price = +newProduct.price
+//         const existingProducts = [...products]
+//         existingProducts.push(newProduct)
+//         setProducts(existingProducts)
+//     };
+
+//     return (
+//         <div className="container">
+//             <NewProduct handleAddProduct={handleAddProduct}  />
+//             <Products products={products} />
+//         </div>
+        
+//     );
+// };
+
+// export default App;
+import React from 'react'
+
+
+import Form from './component/Form'
 
 const App = () => {
-    return (
-        <div>
-            <h1>Photo-Album-React-Project  </h1>
-            <p> contributors: Mercy, Enobong and BabaTunde</p>
-        </div>
-    );
-};
+  return (
+    <div>
+     <Form />
+    </div>
+  )
+}
 
-export default App;
+export default App
